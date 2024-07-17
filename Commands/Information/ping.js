0ee53db41198d4a3be1f089da2ebf4ae6b089dcb
NsGames07 <106678739+DevNsGames07@users.noreply.github.com>
2024-07-17 14:30:26 +0200

    ping command

---
+++ Commands/Information/ping.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('testing')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction, client) {
+
+    const embedje = new EmbedBuilder()
+    .setColor("White")
+    .setDescription(`Pinging....`)
+    .setFooter({ text: `Powered by ${client.user.username}`, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+    .setTimestamp()
+
+    const sent = await interaction.reply({ embeds: [embedje], fetchReply: true })
+
+    const embedje2 = new EmbedBuilder()
+    .setColor("White")
+    .setDescription(`Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`)
+    .setFooter({ text: `Powered by ${client.user.username}`, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+    .setTimestamp()
+
+    interaction.editReply({ embeds: [embedje2] });
+
+
+  },
+};
